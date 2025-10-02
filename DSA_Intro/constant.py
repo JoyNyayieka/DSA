2d92daeba37a267701e638ae3fba22efcce51a6b
Mitch Nyayieka <joynyayieka@gmail.com>
2025-10-02 12:53:19 +0300

    Add array and sorting algo

---
+++ DSA_Intro/constant.py
@@ -0,0 +1,18 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+# for i in numbers:
+#     for j in numbers:
+#         print(i, j)
+        
+        
+# name = "Joy Nyayieka"
+# for x in name:
+#     for y in name:
+#         print(x, y)
+        
+
+position  = [1,2,3,4,5]
+for num in position:
+    print(num)
+    for index in position:
+        print({index})
+        
